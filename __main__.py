f4bd0f01b60e24678b6fb0fbabcc7e48d77a607d
Simon StJG <Simon.StJohn-Green@softwire.com>
2015-11-02 21:49:34 +0000

    Fix PEP008 violations.
    Includes converting all tabs to spaces.

--- __main__.py
+++ __main__.py
@@ -1,4 +1,3 @@
-
 import sys
 import getopt
 import ConfigParser
@@ -8,50 +7,52 @@ from probe import OrgProbe
 from api import APIRequest
 
 optlist, optargs = getopt.getopt(sys.argv[1:],
-	'c:v',
-	['register','email=','secret=','seed=']
-	)
+                                 'c:v',
+                                 ['register', 'email=', 'secret=', 'seed=']
+                                 )
 opts = dict(optlist)
 logging.basicConfig(
-	level = logging.DEBUG if '-v' in opts else logging.INFO,
-	datefmt = '[%Y-%m-%d %H:%M:%S]',
-	format='%(asctime)s\t%(name)s\t%(levelname)s\t%(message)s')
+    level=logging.DEBUG if '-v' in opts else logging.INFO,
+    datefmt='[%Y-%m-%d %H:%M:%S]',
+    format='%(asctime)s\t%(name)s\t%(levelname)s\t%(message)s')
 
 logging.getLogger('urllib3.connectionpool').setLevel(logging.ERROR)
-logging.getLogger('requests.packages.urllib3.connectionpool').setLevel(logging.ERROR)
+logging.getLogger('requests.packages.urllib3.connectionpool').setLevel(
+    logging.ERROR)
 
-configfile = opts.get('-c','config.ini')
+configfile = opts.get('-c', 'config.ini')
 config = ConfigParser.ConfigParser()
 loaded = config.read([configfile])
 logging.info("Loaded %s config files from %s", loaded, configfile)
 
 if not config.has_section('global'):
-	config.add_section('global')
-	config.set('global','interval',1)
-	with open(configfile,'w') as fp:
-		config.write(fp)
+    config.add_section('global')
+    config.set('global', 'interval', 1)
+    with open(configfile, 'w') as fp:
+        config.write(fp)
 
 if config.has_section('api'):
-	def apiconfig(prop, method):
-		try:
-			setattr(APIRequest, prop.upper(), method('api',prop))
-			logging.info("Set %s to %s", prop, getattr(APIRequest, prop.upper()))
-		except Exception,v:
-			pass
+    def apiconfig(prop, method):
+        try:
+            setattr(APIRequest, prop.upper(), method('api', prop))
+            logging.info("Set %s to %s", prop,
+                         getattr(APIRequest, prop.upper()))
+        except Exception:
+            pass
 
-	apiconfig('https', config.getboolean)
-	apiconfig('host', config.get)
-	apiconfig('port', config.getint)
-	apiconfig('version', config.get)
+    apiconfig('https', config.getboolean)
+    apiconfig('host', config.get)
+    apiconfig('port', config.getint)
+    apiconfig('version', config.get)
 
 probe = OrgProbe(config)
 
 if '--register' in opts:
-	probe.register(opts, config)
-	with open(configfile,'w') as fp:
-		config.write(fp)
-	sys.exit(0)
+    probe.register(opts, config)
+    with open(configfile, 'w') as fp:
+        config.write(fp)
+    sys.exit(0)
 
 else:
-	logging.info("Entering run mode")
-	sys.exit(probe.run(optargs))
+    logging.info("Entering run mode")
+    sys.exit(probe.run(optargs))
--- signing.py
+++ signing.py
@@ -3,18 +3,20 @@ import hashlib
 import logging
 import datetime
 
+
 class RequestSigner(object):
-	def __init__(self, secret):
-		self.secret = secret
+    def __init__(self, secret):
+        self.secret = secret
 
-	def sign(self, *args):
-		msg = ':'.join([str(x) if not isinstance(x, (str,unicode)) else x for x in args])
-		logging.debug("Using signature string: %s", msg)
-		hm = hmac.new(self.secret, msg, hashlib.sha512)
-		return hm.hexdigest()
+    def sign(self, *args):
+        msg = ':'.join(
+            [str(x) if not isinstance(x, (str, unicode)) else x for x in args])
+        logging.debug("Using signature string: %s", msg)
+        hm = hmac.new(self.secret, msg, hashlib.sha512)
+        return hm.hexdigest()
 
-	def get_signature(self, args, keys):
-		return self.sign(*[args[x] for x in keys])
+    def get_signature(self, args, keys):
+        return self.sign(*[args[x] for x in keys])
 
-	def timestamp(self):
-		return datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
+    def timestamp(self):
+        return datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
