3493b44a461e0b121c6d2667b94343030bd98a86
Daniel Ramsay <daniel@dretzq.org.uk>
2017-02-16 01:23:55 +0000

    add configure script, to setup probe from docker env vars

--- Dockerfile
+++ Dockerfile
@@ -14,6 +14,7 @@ RUN mkdir /usr/local/probe
 COPY *.py /usr/local/probe/
 COPY docker/run-probe.sh /usr/local/probe/run-probe.sh
 COPY docker/config.ini.tmpl /usr/local/probe/config.ini.tmpl
+COPY docker/configure.py /usr/local/probe/configure.py
 
 RUN chmod a+x /usr/local/probe/run-probe.sh
 
--- docker/run-probe.sh
+++ docker/run-probe.sh
@@ -5,34 +5,7 @@ CONFIG=$PRBDIR/config.ini
 
 if [ ! -f $CONFIG ] 
 then
-  [ -z "$PROBE_UUID" -o -z "$PROBE_SECRET" -o -z "$AMQP_USER" -o -z "$AMQP_PASSWD" ] && \
-    echo "Required envvars: PROBE_UUID, PROBE_SECRET, AMQP_USER, AMQP_PASSWD." && \
-    exit 1
-
-  # use API_HOST if defined
-  API_HOST=${API_HOST:-api.blocked.org.uk}
-  API_PORT=${API_PORT:-443}
-  API_HTTPS=${API_HTTPS:-True}
-
-  # create config file from template
-  cp $PRBDIR/config.ini.tmpl $CONFIG
-  sed -i $CONFIG \
-    -e "s/API_HOST/$API_HOST/" \
-    -e "s/API_PORT/$API_PORT/" \
-    -e "s/API_HTTPS/$API_HTTPS/" \
-    -e "s/PROBE_UUID/$PROBE_UUID/" \
-    -e "s/PROBE_SECRET/$PROBE_SECRET/" \
-    -e "s/AMQP_USER/$AMQP_USER/" \
-    -e "s/AMQP_PASSWD/$AMQP_PASSWD/" 
-
-  if [ ! -z "$REDIS" ]
-  then
-    PROBE_LIMIT=${PROBE_LIMIT:-200000000}
-    echo "limit = $PROBE_LIMIT" >> $CONFIG
-    echo "" >> $CONFIG
-    echo "[accounting]" >> $CONFIG
-    echo "redis_server = $REDIS" >> $CONFIG
-  fi
+  python $PRBDIR/configure.py -o $CONFIG
 fi
 
 exec /usr/bin/python $PRBDIR/__main__.py -c $PRBDIR/config.ini
