a510b38e313e67a7bb5bd9921e600b50e3408410
Daniel Ramsay <daniel@dretzq.org.uk>
2017-08-30 21:21:01 +0100

    conditional requests test

--- tests/test_probe.py
+++ tests/test_probe.py
@@ -4,6 +4,7 @@ from OrgProbe.match import RulesMatcher
 
 import unittest
 import logging
+import requests
 #logging.basicConfig(level=logging.DEBUG)
 
 class ProbeTests(unittest.TestCase):
@@ -12,19 +13,24 @@ class ProbeTests(unittest.TestCase):
         self.probe.probe = {}
         self.probe.rules_matcher = RulesMatcher([],[],[])
 
+        # test that requests library is new enough
+        self.requests_new = map(int, requests.__version__.split('.')) > [2,12,0]
+
     def testRetrieveNotExistant(self):
 
         result = self.probe.test_url('http://does.not.exist.example.com')
         self.assertEquals(result.status, 'dnserror')
         self.assertEquals(result.code, -1)
-        self.assertEquals(result.ip, None)
+        if self.requests_new:
+            self.assertEquals(result.ip, None)
 
     def testRetrieveInvalid(self):
 
         result = self.probe.test_url('bob')
         self.assertEquals(result.status, 'error')
         self.assertEquals(result.code, -1)
-        self.assertEquals(result.ip, None)
+        if self.requests_new:
+            self.assertEquals(result.ip, None)
         
     def testNoHTTPS(self):
 
@@ -33,7 +39,8 @@ class ProbeTests(unittest.TestCase):
         self.assertEquals(result.code, 200)
         self.assertEquals(result.ssl_verified, None)
         self.assertEquals(result.ssl_fingerprint, None)
-        self.assertNotEquals(result.ip, None)
+        if self.requests_new:
+            self.assertNotEquals(result.ip, None)
 
     def testHTTPS(self):
 
@@ -51,3 +58,5 @@ class ProbeTests(unittest.TestCase):
         self.assertEquals(result.code, -1)
         self.assertEquals(result.ssl_verified, None)
         self.assertEquals(result.ssl_fingerprint, None)
+
+
