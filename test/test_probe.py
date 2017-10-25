bc633ced07274d24a650f3b09e8bfc0a2d74db89
Daniel Ramsay <daniel@dretzq.org.uk>
2017-10-26 00:24:25 +0100

    add tests for setup of redis request counting

--- test/test_probe.py
+++ test/test_probe.py
@@ -1,4 +1,5 @@
 import logging
+import configparser
 
 import pytest
 from OrgProbe.probe import Probe
@@ -16,6 +17,37 @@ def probe():
     probe.probe = {}
     probe.rules_matcher = RulesMatcher([], [], [])
     return probe
+
+def test_config_no_accounting_section(probe):
+    probe.config = configparser.ConfigParser()
+    probe.setup_accounting()
+    assert probe.counters is None
+
+def test_config_no_accounting_key(probe):
+    probe.config = configparser.ConfigParser()
+    probe.config.read_string(u"""[accounting]""")
+    probe.setup_accounting()
+    assert probe.counters is None
+
+def test_config_no_accounting_value(probe):
+    probe.config = configparser.ConfigParser()
+    probe.config.read_string(u"""[accounting]\nredis_server=""")
+    probe.setup_accounting()
+    assert probe.counters is None
+
+def test_config_with_accounting(probe, mocker):
+    Accounting = mocker.patch('OrgProbe.probe.Accounting')
+
+    instance = Accounting.return_value
+
+    probe.config = configparser.ConfigParser()
+    probe.config.read_string(u"""[accounting]\nredis_server=foo""")
+    probe.isp = "ExampleISP"
+
+    probe.setup_accounting()
+
+    Accounting.assert_called_with(probe.config, "exampleisp", probe.probe)
+    assert probe.counters is instance
 
 
 def test_retrieve_not_existent(probe):
