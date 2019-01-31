abf8d217ca44bd9b62fe3cb88b0a1fdc22d6826e
Daniel Ramsay <daniel@dretzq.org.uk>
2019-01-31 20:01:14 +0000

    add port and vhost parameters

--- orgprobe/amqpqueue.py
+++ orgprobe/amqpqueue.py
@@ -12,6 +12,8 @@ class AMQPQueue(object):
         self.params = pika.ConnectionParameters(
             host=opts['host'],
             credentials=creds,
+            virtual_host=opts.get('vhost', '/'),
+            port=int(opts.get('port', 5672)),
             heartbeat_interval=15,
         )
         self.network = network
