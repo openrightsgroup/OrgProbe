4093ba1c01617ed533d4b43b4e874477e7ef4bf5
Daniel Ramsay <daniel@dretzq.org.uk>
2024-03-11 19:39:16 +0000

    start of py3 test fixes

--- test/mock_server.py
+++ test/mock_server.py
@@ -1,16 +1,16 @@
-import BaseHTTPServer
-import SocketServer
+import http.server
+import socketserver
 import os
 import ssl
 from contextlib import contextmanager
-import thread
+import threading
 
 
 CERTIFICATE_FINGERPRINT = \
     "42:6D:9E:65:5A:25:2A:DA:23:EE:FC:60:22:5C:51:91:6E:C1:8E:45:7C:C5:05:12:56:C5:D3:CD:7E:74:A0:B9"
 
 
-class HttpHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+class HttpHandler(http.server.BaseHTTPRequestHandler):
     # Probe requires connections to be kept alive (eg. usage of connection.sock),
     # this is easiest to do via HTTP/1.1.
     protocol_version = 'HTTP/1.1'
@@ -103,7 +103,7 @@ def https_server_that_returns_success():
                             'ssl_certs',
                             'localhost.crt')
 
-    server = BaseHTTPServer.HTTPServer(("localhost", 0), HttpHandler)
+    server = http.server.HTTPServer(("localhost", 0), HttpHandler)
     server.socket = ssl.wrap_socket(server.socket,
                                     keyfile=keyfile,
                                     certfile=certfile,
@@ -117,9 +117,9 @@ def https_server_that_returns_success():
 
 @contextmanager
 def http_server_that_returns_success():
-    server = BaseHTTPServer.HTTPServer(("localhost", 0), HttpHandler)
+    server = http.server.HTTPServer(("localhost", 0), HttpHandler)
     try:
-        thread.start_new_thread(server.handle_request, ())
+        threading.Thread(server.handle_request, ())
         yield server.server_address[1]
     finally:
         server.server_close()
@@ -128,11 +128,11 @@ def http_server_that_returns_success():
 @contextmanager
 def tcp_server_that_times_out():
 
-    class EmptyHandler(SocketServer.BaseRequestHandler):
+    class EmptyHandler(socketserver.BaseRequestHandler):
         def handle(self):
             pass
 
-    server = SocketServer.TCPServer(("localhost", 0), EmptyHandler)
+    server = socketserver.TCPServer(("localhost", 0), EmptyHandler)
     try:
         yield server.server_address[1]
         # Don't handle any requests!
